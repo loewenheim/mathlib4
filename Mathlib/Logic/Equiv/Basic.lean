47ad8a1e28a4e93686f5bc6d7dbe87be292eccf3
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-01-28 20:20:51 -0500

    Logic.Equiv.Basic

--- Mathlib/Logic/Equiv/Basic.lean
+++ Mathlib/Logic/Equiv/Basic.lean
@@ -1158,7 +1158,8 @@ def sigmaSubtypeEquivOfSubset (p : α → Type v) (q : α → Prop) (h : ∀ x, 
 def sigmaSubtypeFiberEquiv {α β : Type _} (f : α → β) (p : β → Prop) (h : ∀ x, p (f x)) :
     (Σ y : Subtype p, { x : α // f x = y }) ≃ α :=
   calc
-    _ ≃ Σy : β, { x : α // f x = y } := sigmaSubtypeEquivOfSubset _ p fun _ ⟨x, h'⟩ => h' ▸ h x
+    _ ≃ Σy : β, { x : α // f x = y } :=
+          @sigmaSubtypeEquivOfSubset _ (fun y => { x : α // f x = y }) p (fun _ ⟨x, h'⟩ => h' ▸ h x)
     _ ≃ α := sigmaFiberEquiv f
 #align equiv.sigma_subtype_fiber_equiv Equiv.sigmaSubtypeFiberEquiv
 
