74422a6e4f3e1ea9a3ece5db93be993358282ac9
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-01-30 13:15:41 +0100

    automated fixes
    Mathbin -> Mathlib
    
    fix certain import statements
    
    move "by" to end of line
    
    add import to Mathlib.lean

--- Mathlib.lean
+++ Mathlib.lean
@@ -800,6 +800,7 @@ import Mathlib.RingTheory.NonZeroDivisors
 import Mathlib.RingTheory.OreLocalization.OreSet
 import Mathlib.RingTheory.Prime
 import Mathlib.RingTheory.RingInvo
+import Mathlib.RingTheory.Subring.Basic
 import Mathlib.RingTheory.Subsemiring.Basic
 import Mathlib.SetTheory.Cardinal.SchroederBernstein
 import Mathlib.Tactic.Abel
